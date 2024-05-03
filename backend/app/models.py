04e684f611ac12f6ac0b04e96c48f9f1a8bbf65c
YX Lee <89239531+yuxiann@users.noreply.github.com>
2024-05-04 03:45:20 +0800

    add essential models needed for an ecommerce

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,72 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    address = models.CharField(max_length=120)
+    phone = models.CharField(max_length=120)
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=120)
+    address = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+
+
+class Product(models.Model):
+    class CategoryChoices(models.TextChoices):
+        ELECTRONICS = 'Electronics'
+        FASHION = 'Fashion'
+        FURNITURE = 'Furniture & Decor'
+        HEALTH_BEAUTY = 'Health,Personal Care & Beauty'
+        HOUSEHOLD = 'Household Item'
+        BOOKS_GAMES = 'Books,Movies,Music & Games'
+        TOYS = 'Toys & Hobbies'
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.FloatField()
+    stock = models.IntegerField()
+    image = models.ImageField(upload_to='products')
+    category = models.TextField(choices=CategoryChoices.choices)
+    store = models.OneToOneField(Store, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    class StatusChoices(models.IntegerChoices):
+        CANCELLED = 0
+        PENDING = 1
+        PROCESSING = 2
+        SHIPPED = 3
+        DELIVERED = 4
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.FloatField()
+    status = models.IntegerField(StatusChoices, default=StatusChoices.PENDING)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    shipping_address = models.CharField(max_length=120)
+    billing_address = models.CharField(max_length=120)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    review = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('product', 'user')
+        ordering = '-created_at'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
