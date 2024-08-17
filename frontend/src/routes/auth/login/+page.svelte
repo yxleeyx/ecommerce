99b0baaafda4dbee7bb6be8270d1efc5b431a1ad
YX Lee <89239531+yuxiann@users.noreply.github.com>
2024-08-18 00:08:46 +0800

    add login page and route

---
+++ frontend/src/routes/auth/login/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { enhance } from "$app/forms";
+
+</script>
+<h1>Please Login</h1>
+
+<form method="POST">
+    <label>
+        <input name="Username" placeholder="Username"/>
+    </label>
+    <label>
+        Password: <input name="Password" placeholder="Password"/>
+    </label>
+    <button>Login</button>
+</form>
