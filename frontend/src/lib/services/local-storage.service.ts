54bd009a7e2ca5823a1f38d75380faec12896572
YX Lee <89239531+yuxiann@users.noreply.github.com>
2024-09-16 23:26:38 +0800

    configure browser storage for auth tokens

--- frontend/src/lib/services/local-storage.service.ts
+++ frontend/src/lib/services/local-storage.service.ts
@@ -1,31 +1,31 @@
 export const accessTokenKey = "access_token";
 export const refreshTokenKey = "refresh_token";
 
-export function saveAuthTokens(accessToken, refreshToken) {
-    localStorage.setItem(accessTokenKey, accessToken);
-    localStorage.setItem(refreshTokenKey, refreshToken);
+export function saveAuthTokens(accessToken: string, refreshToken: string) {
+	localStorage.setItem(accessTokenKey, accessToken);
+	localStorage.setItem(refreshTokenKey, refreshToken);
 }
 
-export function getAccessToken(): {
-    try {
-        return localStorage.getItem(accessTokenKey);
-    } catch {
-        return null;
-    }
+export function getAccessToken(): string | null {
+	try {
+		return localStorage.getItem(accessTokenKey);
+	} catch {
+		return null;
+	}
 }
 
-export function getRefreshToken(): {
-    try {
-        return localStorage.getItem(refreshTokenKey);
-    } catch {
-        return null;
-    }
+export function getRefreshToken(): string | null {
+	try {
+		return localStorage.getItem(refreshTokenKey);
+	} catch {
+		return null;
+	}
 }
 
-export function saveNewAccessToken(newToken) {
-    localStorage.setItem(accessTokenKey, newToken);
+export function saveNewAccessToken(newToken: string) {
+	localStorage.setItem(accessTokenKey, newToken);
 }
 
-export function clearAuthTokens() {
-    localStorage.clear()
+export function clearAuthTokens(): void {
+	localStorage.clear();
 }
