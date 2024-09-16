bd906a6c69bbb137ce504bbba944499efb413070
YX Lee <89239531+yuxiann@users.noreply.github.com>
2024-09-16 23:29:34 +0800

    create landing page, login page and dashboard page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { goto } from "$app/navigation";
+	import { setContextClient } from "@urql/svelte";
+
+	import gqlClient from "../lib/services/base.service";
+	import { getAccessToken } from "$lib/services/local-storage.service";
+
+	onMount(() => {
+		const accessToken = getAccessToken();
+		const visitingRoute = location.pathname;
+
+		if (!accessToken) return goto(`/auth/login`);
+
+		//  if (visitingRoute === '/') return goto(`/dashboard`);
+	});
+
+	setContextClient(gqlClient);
+</script>
+
+<p>loading...</p>
+
 <h1>Welcome to SvelteKit</h1>
 <a href="/auth/login">Login</a>
--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -1,15 +1,57 @@
-<script>
-	import { enhance } from "$app/forms";
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { useForm, Hint, validators, email, required, HintGroup } from "svelte-use-form";
 
+	import authService from "$lib/services/auth.service";
+	import { saveAuthTokens } from "$lib/services/local-storage.service";
+
+	const form = useForm();
+
+	let username: string;
+	let password: string;
+	let isLoading = false;
+
+	function handleError(error: any) {
+		if (typeof error === "string") {
+			console.log("Login Error", error);
+		} else if (error instanceof Error) {
+			console.log("Login Error", error.message);
+		}
+	}
+
+	async function onLogin() {
+		if (isLoading) return;
+		try {
+			isLoading = true;
+			const data = await authService.login(username, password);
+			saveAuthTokens(data!.tokenAuth.token as string, data!.tokenAuth.refreshToken as string);
+			goto("/dashboard");
+		} catch (e) {
+			handleError(e);
+		} finally {
+			isLoading = false;
+		}
+	}
 </script>
-<h1>Please Login</h1>
 
-<form method="POST">
-    <label>
-        <input name="Username" placeholder="Username"/>
-    </label>
-    <label>
-        Password: <input name="Password" placeholder="Password"/>
-    </label>
-    <button>Login</button>
-</form>
+<div>
+	<h1>Please Login</h1>
+
+	<form use:form on:submit={onLogin}>
+		<label>
+			Username: <input name="username" placeholder="Username" bind:value={username} use:validators={[required]} />
+			<Hint for="username" on="required">This is a mandatory field</Hint>
+		</label>
+		<label>
+			Password: <input
+				type="password"
+				name="password"
+				placeholder="Password"
+				bind:value={password}
+				use:validators={[required]}
+			/>
+			<Hint for="password" on="required">This is a mandatory field</Hint>
+		</label>
+		<button type="submit" disabled={!$form.valid}>LOGIN</button>
+	</form>
+</div>
